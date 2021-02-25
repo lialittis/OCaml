5bee20f3370b6f0f0ef78b13b053252c2c8ff8df
lialittis <yu-tianchi@outlook.com>
2021-02-25 23:07:55 +0100

    add 08_match

---
+++ 08_match/README.md
@@ -0,0 +1,14 @@
+# Match Expression
+
+It is simiar to the switch expression in JAVA.
+
+## description
+
+* match <expression> with <match>
+* match <expression> with <match>
+	* <pattern 1> -> <expression 1>
+	* | <pattern 2> -> <expression 2>
+	* | ...
+	* | <pattern n> -> <expression n>
+
+
---
+++ 08_match/match.ml
@@ -0,0 +1,42 @@
+(*match expression*)
+
+let int_to_month i =
+  match i with
+    1 -> "Jan"
+  | 2 -> "Feb"
+  | 3 -> "Mar"
+  | 4 -> "Apr"
+  | 5 -> "May"
+  | 6 -> "Jun"
+  | 7 -> "Jul"
+  | 8 -> "Aug"
+  | 9 -> "Sep"
+  | 10 -> "Oct"
+  | 11 -> "Nov"
+  | 12 -> "Dec"
+  | _ -> "None";;
+
+int_to_month 11;;
+
+(*update*)
+
+let int_to_month (i:int) : string =
+  match i with
+    1 -> "Jan"
+  | 2 -> "Feb"
+  | 3 -> "Mar"
+  | 4 -> "Apr"
+  | 5 -> "May"
+  | 6 -> "Jun"
+  | 7 -> "Jul"
+  | 8 -> "Aug"
+  | 9 -> "Sep"
+  | 10 -> "Oct"
+  | 11 -> "Nov"
+  | 12 -> "Dec"
+  | _ -> "None";;
+
+
+
+
+      
