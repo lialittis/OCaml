809fcbc20c75d02944108f67a99ce8fc1ead3c19
lialittis <yu-tianchi@outlook.com>
2021-02-28 02:04:54 +0100

    add 09_system

---
+++ 09_system/README.md
@@ -0,0 +1,5 @@
+# The OCAML System
+
+## Very important reference
+
+https://caml.inria.fr/pub/docs/manual-ocaml/index.html
---
+++ 09_system/system.ml
@@ -0,0 +1,22 @@
+(*open a module*)
+
+(*example*)
+List.hd [1;2;3;4];;
+
+List.tl [1;2;3;4];;
+
+String.get "Test" 3;;(*return the third element*)
+
+String.get "Test" 4;;(*index out of bounds*)
+
+
+module S = String;;
+
+S.sub "this is a test" 2 6;;
+
+
+(*open a module, then we don't need to write String dot*)
+open String;;
+
+get "Test" 3;;
+
